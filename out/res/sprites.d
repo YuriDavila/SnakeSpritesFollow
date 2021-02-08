623889af6e3ee7f282227568d4ef97a3d9d8325b
Yuri d´Ávila <53717855+YuriDavila@users.noreply.github.com>
2021-02-08 16:09:03 -0300

    Delete out directory

--- out/res/sprites.d
+++
@@ -1,3 +0,0 @@
-out/res/sprites.o: res/sprites.res \
-E:/YuriNOVOS/Games/Sega\ Games/Games\ SGDK\ Davila\ Games/ProjetosGames/SampleProjects/SampleSpriteFollow/res/sprites/spr_inim_starblade.png \
-E:/YuriNOVOS/Games/Sega\ Games/Games\ SGDK\ Davila\ Games/ProjetosGames/SampleProjects/SampleSpriteFollow/res/sprites/spr_inim_starblade2.png
